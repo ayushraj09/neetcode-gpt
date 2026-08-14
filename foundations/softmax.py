e447b13c8d6e580dc5555eac542577179838ed0d
Ayush Raj <89580033+ayushraj09@users.noreply.github.com>
2026-08-14 14:56:54 +0530

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -7,7 +7,6 @@ class Solution:
     def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
-        z[:] = z-max(z)
 
         # return np.round(your_answer, 4)
-        return np.round(np.exp(z)/np.sum(np.exp(z)), 4)
+        return np.round(np.exp(z-max(z))/np.sum(np.exp(z-max(z))), 4)
